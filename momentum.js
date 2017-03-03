4d6263cc12063ac17edb8acfd3f40cd179836d97
Gioele Antoci <gioele.antoci@gmail.com>
2017-03-03 09:10:14 -0500

    Delete momentum.js

--- momentum.js
+++
@@ -1,7 +0,0 @@
-fetch('https://jsonplaceholder.typicode.com/users')
-    .then(function (response) {
-    return response.text();
-}).then(function (body) {
-    document.body.innerHTML = body;
-});
-//# sourceMappingURL=momentum.js.map
